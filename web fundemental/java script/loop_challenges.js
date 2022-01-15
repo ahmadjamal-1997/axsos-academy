3b7d49ca8be993ae36e8b4ad58e1ee47af462e4a
ahmadjamal-1997 <ahmad.jamal.200797@gmail.com>
2022-01-15 13:58:17 +0200

    initial commit

---
+++ web fundemental/java script/loop_challenges.js
@@ -0,0 +1,28 @@
+for(i=1;i<21;i++){
+    if(i%2 !=0){
+        console.log(i);
+    }
+}
+//first challenge
+for(var i=100;i>0;i--){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+//second challenge
+for(var i=4;i>=-3.5;i-=1.5){
+    
+    console.log(i);
+}
+//third challenge
+var sum=0
+for(i=1;i<101;i++){
+    sum=sum+i;
+}
+console.log(sum);
+//fourth challenge
+for(i=1;i<13;i++){
+    product=product*i;
+}
+console.log(product);
+//fifth challenge
