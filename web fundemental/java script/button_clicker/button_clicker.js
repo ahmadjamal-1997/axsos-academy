80e114f1cf184c333d2c1bf3dfe89481dc13fdb0
ahmadjamal-1997 <ahmad.jamal.200797@gmail.com>
2022-01-15 17:20:16 +0200

    initial

---
+++ web fundemental/java script/button_clicker/button_clicker.js
@@ -0,0 +1,12 @@
+function changetxt(element){
+    element.innerText = "Logout";
+
+}
+
+function alert(element){
+    console.log("Ninja was liked");
+
+}
+function remove(element){
+    element.remove();
+}
