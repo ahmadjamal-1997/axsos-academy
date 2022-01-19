c8d70658552f26b4d316f24a4cc22fe01b4edbd0
ahmadjamal-1997 <ahmad.jamal.200797@gmail.com>
2022-01-19 16:54:36 +0200

    initial

--- web fundemental/java script/calculator/cal.js
+++ web fundemental/java script/calculator/cal.js
@@ -9,38 +9,36 @@ function press(a){
     newarr1.push(a)
     var c=newarr1.join('')
     element.innerText=c
-
 }
 else {
     newarr2.push(a)
     var o=newarr2.join('')
     element.innerText=o
 }
-
 }
-
 function setOP(operations){
     var b=document.querySelector("#display")
-    var d=parseInt(b.innerText)
+    //var d=parseInt(b.innerText)
     operation=operations
 
 }
 
 function calculate(){
     if(operation=="*"){
-        total=newarr1.join('')*newarr2.join('')
+        total= parseInt(newarr1.join(''))*parseInt(newarr2.join(''))
         element.innerText=total
     }
     else if(operation=="/"){
-        total=newarr1.join('')/newarr2.join('')
+        total=parseInt(newarr1.join(''))/parseInt(newarr2.join(''))
         element.innerText=total
     }
     else if(operation=="+"){
-        total=newarr1.join('')+newarr2.join('')
+        total=parseInt(newarr1.join(''))+parseInt(newarr2.join(''))
+        console.log(newarr1,newarr2)
         element.innerText=total
     }
     else if(operation=="-"){
-        total=newarr1.join('')-newarr2.join('')
+        total=parseInt(newarr1.join(''))-parseInt(newarr2.join(''))
         element.innerText=total
     }
 }
