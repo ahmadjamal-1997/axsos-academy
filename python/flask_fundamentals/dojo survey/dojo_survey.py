f00901b67baf3a7818d40088701b583b98d71f8e
ahmadjamal-1997 <ahmad.jamal.200797@gmail.com>
2022-02-06 14:09:06 +0200

    initial

---
+++ python/flask_fundamentals/dojo survey/dojo_survey.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template,request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("dojo_survey.html")
+
+@app.route('/result',methods=["post"])
+def show():
+    form_name=request.form['username']
+    form_location=request.form['location']
+    form_language=request.form['language']
+    form_comment=request.form ['comment']
+    return render_template('show.html',form_name=form_name,form_location=form_location,form_language=form_language,form_comment=form_comment)
+if __name__ == "__main__":
+    app.run(debug=True)
