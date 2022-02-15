9a55746fcb9806695ad7101d8e0c6ac8a28ca263
ahmadjamal-1997 <ahmad.jamal.200797@gmail.com>
2022-02-15 17:58:14 +0200

    initial

--- python/django/tv_shows/tv_app/models.py
+++ python/django/tv_shows/tv_app/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 
+
+class SHOWManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "title name should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network name should be at least 3 characters"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "the Description should be at least 10 characters"
+        return errors
+
+    def edit_validator(self,postData):
+        errors = {}
+        if len(postData['new_title']) < 2:
+            errors["new_title"] = "title name should be at least 2 characters"
+        if len(postData['new_network']) < 3:
+            errors["new_network"] = "Network name should be at least 3 characters"
+        if len(postData['new_desc']) < 10:
+            errors["new_desc"] = "the Description should be at least 10 characters"
+        return errors
+
+
+
 class SHOW (models.Model):
         title = models.CharField(max_length=255)
         network = models.CharField(max_length=255)
@@ -7,5 +31,6 @@ class SHOW (models.Model):
         desc = models.TextField()
         created_at = models.DateTimeField(auto_now_add=True)
         updated_at = models.DateTimeField(auto_now=True)
+        objects = SHOWManager()
 
 # Create your models here.
--- python/django/tv_shows/tv_app/views.py
+++ python/django/tv_shows/tv_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import SHOW
+from django.contrib import messages
 
 def show(request):
 
@@ -12,8 +13,15 @@ def add(request):
     return render(request,'tv_show.html')
 
 def create(request):
-    this_show=SHOW.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['date'],desc=request.POST['desc'])
-    num=this_show.id
+    errors = SHOW.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/new')
+    else:
+        this_show=SHOW.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['date'],desc=request.POST['desc'])
+        num=this_show.id
+        messages.success(request, "show successfully updated")
     return redirect(f'/{num}')
 
 def details(request,show_id):
@@ -31,16 +39,24 @@ def edit(request,show_id):
     return render(request,'show_edit.html',context)
 
 def update(request,show_id):
-    c=SHOW.objects.get(id=show_id)
-    c.title=request.POST['new_title']
-    c.network=request.POST['new_network']
-    c.date=request.POST['new_date']
-    c.desc=request.POST['new_desc']
-    c.save()
-    return redirect(f'/{show_id}')
+    errors = SHOW.objects.edit_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/new')
+    else:
+        c=SHOW.objects.get(id=show_id)
+        c.title=request.POST['new_title']
+        c.network=request.POST['new_network']
+        c.date=request.POST['new_date']
+        c.desc=request.POST['new_desc']
+        c.save()
+        return redirect(f'/{show_id}')
 
 def delete(request,show_id):
     c=SHOW.objects.get(id=show_id)
     c.delete()
     return redirect('/')
 # Create your views here.
+
+
