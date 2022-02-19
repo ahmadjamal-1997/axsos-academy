085331bb123a6f6f79f6133baf3ad94886f010ad
ahmadjamal-1997 <ahmad.jamal.200797@gmail.com>
2022-02-19 10:37:28 +0200

    final

--- python/django/the_wall/the_wall_app/views.py
+++ python/django/the_wall/the_wall_app/views.py
@@ -5,7 +5,7 @@ from login_app.models import USER,Massage,Comment
 def wall(request):
     context={
         'users' : USER.objects.all(),
-        'massages' : Massage.objects.all(),
+        'massages' : Massage.objects.all().order_by("-created_at"),
         'comments' : Comment.objects.all(),
         'the_user' : USER.objects.get(id=request.session['user_id'])
     }
@@ -13,13 +13,13 @@ def wall(request):
 
 def post(request,the_user_id):
     Massage.objects.create(massage=request.POST['massage'],users=USER.objects.get(id=the_user_id))
-    return redirect (f'/wall')
+    return redirect ('/wall')
 
 def comment(request,msg_id,user_id):
     Comment.objects.create(comment=request.POST['comment'],users=USER.objects.get(id=user_id)
     ,massage=Massage.objects.get(id=msg_id))
     # request.session['this_massage']=msg_id
-    return redirect(f'/wall')
+    return redirect('/wall')
 
 
 
