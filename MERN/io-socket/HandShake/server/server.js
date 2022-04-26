b1a03c9cd54ad8f0cf07e443084aa11153614818
ahmad jamal <ahmad.jamal.200797@gmail.com>
2022-04-26 15:22:25 +0300

    initial

---
+++ MERN/io-socket/HandShake/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { Socket } = require('socket.io');
+const app = express();
+
+const server = app.listen(8000, () =>
+    console.log('The server is all fired up on port 8000')
+);
+
+const io = require('socket.io')(server, { cors: true });
+io.on("connection",Socket=>{
+    Socket.emit("connection",null)
+    Socket.on("message",data=>{
+        console.log(data)
+        Socket.broadcast.emit("message",data)
+    })
+});
